cb1bb7568e6897bfbf66f6b5efdb93891260bd20
MikeEm <michalbanaszek03@gmail.com>
2025-01-11 19:52:15 +0100

    register middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias('auth', \App\Http\Middleware\Authenticate::class);
+        $middleware->alias('auth.basic', \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class);
+        $middleware->alias('bindings', \Illuminate\Routing\Middleware\SubstituteBindings::class);
+        $middleware->alias('cache.headers', \Illuminate\Http\Middleware\SetCacheHeaders::class);
+        $middleware->alias('can', \Illuminate\Auth\Middleware\Authorize::class);
+        $middleware->alias('guest', \App\Http\Middleware\RedirectIfAuthenticated::class);
+        $middleware->alias('signed', \Illuminate\Routing\Middleware\ValidateSignature::class);
+        $middleware->alias('throttle', \Illuminate\Routing\Middleware\ThrottleRequests::class);
+        $middleware->alias('verified', \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class);
+        $middleware->alias('is_admin', \App\Http\Middleware\IsAdmin::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
