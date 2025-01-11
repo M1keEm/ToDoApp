a76037b9342cd9ccd5107236fe3260e2c5e2dfb3
MikeEm <michalbanaszek03@gmail.com>
2025-01-11 19:49:19 +0100

    define route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\toDoListController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', [toDoListController::class, 'index']);
 
 Route::post('/saveItemRoute', [toDoListController::class, 'saveItem'])->name('saveItem');
 
 Route::post('/markCompleteRoute/{id}', [toDoListController::class, 'markComplete'])->name('markComplete');
+
+//Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
