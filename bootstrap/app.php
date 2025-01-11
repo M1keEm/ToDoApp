70ee5691f73dd135c4e16db0c24c988b1677ec75
MikeEm <michalbanaszek03@gmail.com>
2025-01-11 20:09:57 +0100

    app.php alias arguments fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,21 +6,23 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias('auth', \App\Http\Middleware\Authenticate::class);
-        $middleware->alias('auth.basic', \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class);
-        $middleware->alias('bindings', \Illuminate\Routing\Middleware\SubstituteBindings::class);
-        $middleware->alias('cache.headers', \Illuminate\Http\Middleware\SetCacheHeaders::class);
-        $middleware->alias('can', \Illuminate\Auth\Middleware\Authorize::class);
-        $middleware->alias('guest', \App\Http\Middleware\RedirectIfAuthenticated::class);
-        $middleware->alias('signed', \Illuminate\Routing\Middleware\ValidateSignature::class);
-        $middleware->alias('throttle', \Illuminate\Routing\Middleware\ThrottleRequests::class);
-        $middleware->alias('verified', \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class);
-        $middleware->alias('is_admin', \App\Http\Middleware\IsAdmin::class);
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+            'can' => \Illuminate\Auth\Middleware\Authorize::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+            'is_admin' => \App\Http\Middleware\IsAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
